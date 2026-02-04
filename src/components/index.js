892b02affdf5e483ae45beddde02e59b69dbda10
krishnagp97 <krishnapathak4511@gmail.com>
2026-02-04 18:09:10 +0530

    Ignore dist build folder

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
-import Container from "./container/container";
+import Container from "./container/Container.jsx";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
