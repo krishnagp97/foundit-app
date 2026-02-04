18c62dfc77c447117a466cd3269a92831f13a654
krishnagp97 <krishnapathak4511@gmail.com>
2026-02-04 18:26:38 +0530

    Fix all component imports for Vercel Linux build

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
-import Select from "./Select";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Logo from "./Logo";
+import Select from "./Select.jsx";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Logo from "./Logo.jsx";
 import Container from "./container/Container.jsx";
-import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE";
-import Signup from "./Signup";
-import Button from "./button";
-import Input from './Input'
-import Login from "./Login";
-import PostForm from "./post-form/PostForm";
-import PostCard from './PostCard'
-import AuthLayout from './AuthLayout'
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+import Button from "./button.jsx";
+import Input from './Input.jsx'
+import Login from "./Login.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from './PostCard.jsx'
+import AuthLayout from './AuthLayout.jsx'
 export {
     Select,
     Header,
